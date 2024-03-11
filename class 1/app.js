9fb355efc497e03b6a2733d1953d4493318cc2b8
rana934 <asharrana563@gmail.com>
2024-03-12 01:37:28 +0500

    new commit

---
+++ class 1/app.js
@@ -0,0 +1,39 @@
+// alert("assalamualikum world")
+
+
+var salam =("assalamualikum ") 
+var half =("rehmatulahi") 
+var full= ("wabarakatuhu")
+
+prompt(salam +half + full)
+
+// Mathematical Expression
+var string =("abcdefg")
+var integer =(22)
+
+alert (string*integer)
+
+var division =(48)
+var sec_Division =(2)
+
+document.write("<h1>" + division/sec_Division + "</h1>")
+
+
+var multiplication =("89")
+var sec$Multiplication =(65)
+
+console.log (multiplication*sec$Multiplication)
+
+
+var subtraction =("65")
+var $Subtraction =("55")
+
+console.log(subtraction-$Subtraction)
+
+
+var doubleplus =("20")
+console.log (--doubleplus)
+
+
+
+
