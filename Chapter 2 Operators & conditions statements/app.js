b9da24b599113b630839929fe64863c3ea2574cc
rana934 <asharrana563@gmail.com>
2024-04-02 15:06:21 +0500

    new commit

---
+++ Chapter 2 Operators & conditions statements/app.js
@@ -0,0 +1,229 @@
+// ====================================
+// Operaters and conditions statements
+// ====================================
+
+// "Artimetic Operator"
+
+// "Addition"
+{
+    a = 5;
+    b = 5;
+
+    console.log(">>>=============<<<");
+    console.log("Addition");
+    console.log(">>>=============<<<");
+    console.log("a =", a);
+    console.log("b =", b);
+    console.log("a + b =", a + b);
+
+};
+
+//  "Subtraction"
+{
+    c = 5;
+    d = 5;
+
+    console.log(">>>=============<<<");
+    console.log("Subtaction");
+    console.log(">>>=============<<<");
+    console.log("c - =", c);
+    console.log("d =", d);
+    console.log("c - d =", c - d);
+
+};
+
+
+
+//  "Division"
+{
+    C = 5;
+    D = 5;
+
+    console.log(">>>=============<<<");
+    console.log("Division");
+    console.log(">>>=============<<<");
+    console.log("C =", C);
+    console.log("D =", D);
+    console.log("D / D =", C / D);
+
+};
+
+
+// "Multiplication"
+
+{
+    E = 5;
+    F = 5;
+
+    console.log(">>>=============<<<");
+    console.log("Multiplication");
+    console.log(">>>=============<<<");
+    console.log("E =", E);
+    console.log("F =", F);
+    console.log("E * F =", E * F);
+
+};
+
+// "Modules"
+
+{
+    G = 5;
+    H = 5;
+
+    console.log(">>>=============<<<");
+    console.log("Modules");
+    console.log(">>>=============<<<");
+    console.log("G =", G);
+    console.log("H =", H);
+    console.log("G % H =", G % H);
+
+};
+
+
+// "Exponential"
+
+{
+    console.log(">>>=============<<<");
+    console.log("Exponential");
+    console.log(">>>=============<<<");
+    console.log(">>---------<<");
+    console.log("2 ki power 4");
+    console.log(">>---------<<");
+    I = 2;
+    J = 4;
+
+    console.log(I ** J);
+
+}
+
+
+
+// "Unary Operator"
+
+{
+    console.log(">>>=============<<<");
+    console.log("Unary Operator");
+    console.log(">>>=============<<<");
+
+    k = 5
+
+    console.log("k =", k);
+    console.log(k + 1);
+    console.log(">>---------<<");
+    console.log("Second Method");
+    console.log(">>---------<<");
+
+    console.log(">>>=============<<<");
+    console.log("Unary Operator");
+    console.log(">>>=============<<<");
+
+    // 1 (Post_Increment) means >>>> a++;
+    // 1 (Pre_Increment)  means >>>> ++a;
+
+    console.log(" 'Post' ");
+    l = 5;
+
+    console.log(l++);
+
+    console.log(" 'Pre' +");
+    console.log(++l);
+
+};
+
+
+// "Assignment Operator"
+
+// "equals to ="
+
+{
+    console.log(">>>===================<<<");
+    console.log("Assignment Operator '='")
+    console.log(">>>===================<<<");
+
+    M = 5;
+    console.log("M =", 5);
+
+
+    // "Addition Equals to '+=' "
+
+    console.log(">>>=============<<<");
+    console.log("A S Operator'+='")
+    console.log(">>>=============<<<");
+
+    N = 5;
+    console.log("N =", N);
+
+    console.log("N+=", N)
+    console.log(N += 5);
+
+
+
+    // "Subtraction Equals to '-=' "
+
+    console.log(">>>=============<<<");
+    console.log("A S Operator'-='")
+    console.log(">>>=============<<<");
+
+    O = 5;
+    console.log("O =", O);
+
+    console.log("O -=", O);
+    console.log(O -= 5);
+
+
+
+    // "Astirisk Equals to '-=' "
+
+    console.log(">>>=============<<<");
+    console.log("A S Operator '*='")
+    console.log(">>>=============<<<");
+
+    P = 5;
+    console.log("P =", P);
+
+    console.log("P *=", P);
+    console.log(P *= 5);
+
+
+
+     // "Division Equals to '-=' "
+
+     console.log(">>>=============<<<");
+     console.log("A S Operator '/='")
+     console.log(">>>=============<<<");
+ 
+     Q = 5;
+     console.log("Q =", Q);
+ 
+     console.log("Q /=", Q);
+     console.log(Q /= 5);
+
+
+
+     // "Modules Equals to '-=' "
+
+     console.log(">>>=============<<<");
+     console.log("A S Operator '%='")
+     console.log(">>>=============<<<");
+ 
+     R = 5;
+     console.log("R =", R);
+ 
+     console.log("R %=", R);
+     console.log(R %= 5);
+
+
+
+     // "Expponential Equals to '-=' "
+
+     console.log(">>>=============<<<");
+     console.log("A S Operator '**='")
+     console.log(">>>=============<<<");
+ 
+     S = 5;
+     console.log("S =", S);
+ 
+     console.log("S **=", S);
+     console.log(S **= 5);
+
+}
--- class 3/app.js
+++ class 3/app.js
@@ -2,17 +2,17 @@
         // OBJECT
 // >>>>==============<<<<
 
-const student = {
-    fullName : "Rana Muhammad Ashar",
-    age : 17,
-    cgpa : 589,
-    ispass : true,
+// const student = {
+//     fullName : "Rana Muhammad Ashar",
+//     age : 17,
+//     cgpa : 589,
+//     ispass : true,
 
-};
+// };
 
-student ['age'] = student ['age'] + 2
+// student ['age'] = student ['age'] + 2
 
-document.write (student['age']);
+// document.write (student['age']);
 
 // let a= "bcue";
 // a = "hudhu"
@@ -21,7 +21,103 @@ document.write (student['age']);
  
 
 
-student ['fullName'] = student ['fullName'] + 43 ;
+// student ['fullName'] = student ['fullName'] + 43 ;
 
 // student ['fullName'] ;
 // document.write  (student['fullName']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// There are three types of variable keywords 
+// var
+// let
+// const
+
+// var Example: 
+// var name ="ashar"
+// var name ="rana"
+
+// console.log(name)
+
+
+// let name ="ashar";
+//  name="rana";
+// let b;
+// b=32
+// console.log(name)
+// console.log(b)
+ 
+
+
+// const name ="ashar"
+// name ="rana"
+// const a;
+// const a ="ashar"
+// a ="rana"
+
+// console.log(name)
+
+
+// =====================
+// datatypes of javascript
+// =====================
+
+// Number, srring, bolleon, undefined, null, bright, symbol
+
+// let a =21   (type of ["numer"])
+// let b =" ashar"     (type of ["sirng"])
+// let c= true     // (type of ["bolleon"])
+// let d;    (type of ["undeifined"])
+// let b= BigInt (123)      (type of ["bigInt"])
+// let e= Symbol("hello")
+
+
+
+// =====================
+//OBJECTS
+// =====================
+
+// let student ={
+//     fullName :"rana muhammad ashar",
+//     age:17,
+//     class: 10,
+//     rollNUmber: 217369,
+
+// };
+
+// student.age =student.age + 4;
+// student.age= 17;
+// student.fullName = "rana MUHAMMAD sufiyan"
+// student.age = student.age+1
+ 
+
+// practice
+const product = {
+    name: "paker jotter standard ct ball pen (black)",
+    ratting: 7002 ,
+    price:"270$",
+    offer: "5% off",
+
+};
+
+
+const profile = {
+    userName : "rana muhammad ashar",
+    isfollow : true,
+    posts : 10,
+    followers : 413 ,
+    following : 3
+
+};
+
+console.log (typeof profile['followers'])
